686cd31efa4bfc2ef2783c6e85452370b1c2f41d
mburzhinsky-hub <m.burzhinsky@visuals.ru>
2026-06-25 14:43:13 +0300

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/dream_pet_v2/',
   plugins: [react()],
-  base: '/dream_pet/'
 })
